cdb25223b66ae89c9e61779e454f5f12244a380c
Edmund Ju <edmundju@berkeley.edu>
2023-10-04 15:33:06 -0700

    post route working

--- express-consistent-hashing/app.js
+++ express-consistent-hashing/app.js
@@ -6,12 +6,23 @@ const app = express();
 const PORT = process.env.PORT
 const NODES = ["c1:5002", "c2:5003", "c3:5004", "c4:5005", "c5:5006", "c6:5007", "c7:5008", "c8:5009", "c9:5010", "c10:5011"]
 
+const DATA_OBJECT = {}
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200);
     res.send("test")
 })
+
+app.post('/add', (req, res) => {
+    const key = req.body.key
+    const value = req.body.key
+
+    console.log(key, value)
+})
+
+
 
 
 
--- express-consistent-hashing/consistent-hashing.js
+++ express-consistent-hashing/consistent-hashing.js
@@ -1,1 +1,21 @@
 const crypto = require("crypto");
+
+class ConsistentHashing {
+    constructor(nodes) {
+        this.nodes = nodes
+    }
+
+    // Calculate the hash value of a given key
+    calculateHash(key) {
+        const hash = crypto.createHash('md5');
+        hash.update(key);
+        return parseInt(hash.digest('hex'), 16);
+    }
+
+    // Find node responsible for given key using consistent hashing
+    findNode(key) {
+        const hash = this.calculateHash(key);
+        const nodeIndex = hash % this.nodes.length;
+        return this.nodes[nodeIndex]
+    }
+}
